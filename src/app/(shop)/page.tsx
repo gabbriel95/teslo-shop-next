a5524aeaa7183cc4d7163b7d49196da0ef0fb51e
Gabriel <juanmendez0013@gmail.com>
2024-06-26 13:07:28 -0300

    estructura de directorios

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
+    </div>
+    
+  );
+}
