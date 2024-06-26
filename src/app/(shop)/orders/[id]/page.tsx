895d7433c60566b896f5bfb745675bd6a81338b5
Gabriel <juanmendez0013@gmail.com>
2024-06-26 13:27:58 -0300

    rutas de la app

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div>
+      <h1>Orden #avc</h1>
+    </div>
+  );
+}
---
+++ src/app/(shop)/products/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div>
+      <h1>Prtoducts Page</h1>
+    </div>
+  );
+}
