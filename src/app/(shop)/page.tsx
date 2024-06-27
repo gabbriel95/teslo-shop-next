9855e09f4dc74c35bb3c1f5479c8a81b267cb9d4
Gabriel <juanmendez0013@gmail.com>
2024-06-27 17:01:00 -0300

    feat:top menu - products and menu

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,13 @@
-import { titleFont } from "@/config/fonts";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
-    </div>
-    
+    <>
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+      <ProductGrid products={products} />
+    </>
   );
 }
