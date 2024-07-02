6e4bcb144357093c01dd4b8a46efdcba4fba3af8
Gabriel <juanmendez0013@gmail.com>
2024-07-02 15:18:25 -0300

    feat: pantalla de verificacion de compra static

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,106 @@
+import Link from "next/link";
+
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+
+const productInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function () {
   return (
-    <div>
-      <h1>checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar orden" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/*Carrito */}
+
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar elementos</span>
+            <Link href="/cart" className="underline mb-5">
+              Editar carrito
+            </Link>
+            {/*Items */}
+            {productInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} x 3</p>
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/*Checkout - resumen de orden*/}
+
+          <div className="bg-white rounded-r-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Direccion de entrega</h2>
+            <div className="mb-10">
+              <p className="text-xl">Fernando Herrera</p>
+              <p>Av. SIempre viva 123</p>
+              <p>Col.centro</p>
+              <p>Alcaldia test</p>
+              <p>Ciudad de buenos aires</p>
+              <p>CP 1991</p>
+              <p>123.123.123</p>
+            </div>
+
+            {/*Divider */}
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10 " />
+
+            <h2 className="text-2xl mb-02">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 articulos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$100</span>
+
+              <span>Impuestos(15%)</span>
+              <span className="text-right">$100</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-2xl text-right">$100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                {/*Disclaimer */}
+                <span className="text-xs">
+                  Al hacer click en 'colocar orden', aceptas nuestros
+                  <a href="#" className="underline">
+                    terminos y condiciones
+                  </a>
+                  y <a className="underline"> politica de privacidad</a>
+                </span>
+              </p>
+              <Link
+                className="flex btn-primary justify-center"
+                href="/orders/123"
+              >
+                Colocar orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
