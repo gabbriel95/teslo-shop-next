c6647ea259f04286249cc1eed8cb2b48a2097472
Gabriel <juanmendez0013@gmail.com>
2024-07-08 12:58:41 -0300

    fix: custom elements of swiper

---
+++ custom.d.ts
@@ -0,0 +1,7 @@
+// custom.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    "swiper-container": any;
+    "swiper-slide": any; // Puedes añadir otros elementos personalizados aquí si los usas
+  }
+}
