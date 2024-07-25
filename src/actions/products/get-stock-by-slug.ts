910b9fdfae1a912bb50dd61f75e2fcbbc3e80f27
Gabriel <juanmendez0013@gmail.com>
2024-07-25 09:47:17 -0300

    feat: optimizacion de la metadata

--- src/actions/products/get-stock-by-slug.ts
+++ src/actions/products/get-stock-by-slug.ts
@@ -1,9 +1,11 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+//import { sleep } from "@/utils";
 
 export const getStockBySlug = async (slug: string): Promise<number> => {
   try {
+    //await sleep(3);
     const stock = await prisma.product.findFirst({
       select: {
         inStock: true,
