d5ae2dba905ebd9a2650292afbc6dee822c46d76
Gabriel <juanmendez0013@gmail.com>
2024-07-30 16:27:51 -0300

    feat: carrito de compra terminado

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { QuantitySelector, SizeSelector } from "@/components";
-import { Product, Size } from "@/interfaces";
+import type { CartProduct, Product, Size } from "@/interfaces";
+import { useCartStore } from "@/store";
 import React, { useState } from "react";
 
 interface Props {
@@ -9,6 +10,8 @@ interface Props {
 }
 
 export const AddToCart = ({ product }: Props) => {
+  const addProductToCart = useCartStore((state) => state.addProductToCart);
+
   const [size, setSize] = useState<Size | undefined>();
 
   const [quantity, setQuantity] = useState<number>(1);
@@ -17,8 +20,25 @@ export const AddToCart = ({ product }: Props) => {
 
   const addToCart = () => {
     setPosted(true);
+
     if (!size) return;
-    console.log({ size, quantity });
+
+    // console.log({ size, quantity, product });
+
+    const cartProduct: CartProduct = {
+      id: product.id,
+      slug: product.slug,
+      title: product.title,
+      price: product.price,
+      quantity: quantity,
+      size: size,
+      image: product.images[0],
+    };
+    // Todos: add to cart
+    addProductToCart(cartProduct);
+    setPosted(false);
+    setQuantity(1);
+    setSize(undefined);
   };
 
   return (
