d166cce63ee8bc674141957345f13bb70b153ba8
Gabriel <juanmendez0013@gmail.com>
2024-08-06 16:17:47 -0300

    fix: comentarios

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -18,7 +18,7 @@ export const authConfig: NextAuthConfig = {
       return token;
     },
     session({ session, token, user }) {
-      console.log({ session, token, user });
+      //console.log({ session, token, user });
       session.user = token.data as any;
       return session;
     },
