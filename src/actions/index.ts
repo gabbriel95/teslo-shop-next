2ae4ba2fab003a69b96666995e895894cce1370d
Gabriel <juanmendez0013@gmail.com>
2024-08-06 16:43:40 -0300

    feat: fix importaciones

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,8 @@
-export * from "./auth/login";
-export * from "./auth/logout";
-export * from "./auth/registerUser";
+// En ./src/actions/index.ts
+export { login, authenticate } from "./auth/login";
+export { logout } from "./auth/logout";
+export { registerUser } from "./auth/registerUser";
 
-export * from "./products/product-pagination";
-export * from "./products/get-product-by-slug";
-export * from "./products/get-stock-by-slug";
+export { getPaginatedProductsWithImages } from "./products/product-pagination";
+export { getProductBySlug } from "./products/get-product-by-slug";
+export { getStockBySlug } from "./products/get-stock-by-slug";
