4727d0a70befb402f838481297542c3dab2b39ad
Gabriel <juanmendez0013@gmail.com>
2024-08-07 00:01:48 -0300

    feat: agregamos paises al seed, formulario y bd

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,8 @@ export { login, authenticate } from "./auth/login";
 export { logout } from "./auth/logout";
 export { registerUser } from "./auth/registerUser";
 
+export { getCountries } from "./country/get-countries";
+
 export { getPaginatedProductsWithImages } from "./products/product-pagination";
 export { getProductBySlug } from "./products/get-product-by-slug";
 export { getStockBySlug } from "./products/get-stock-by-slug";
