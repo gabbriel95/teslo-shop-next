9c2e135384527efec0bfef774ef1b4e64006e3a8
Gabriel <juanmendez0013@gmail.com>
2024-08-07 20:32:27 -0300

    feat: elementos de la orden

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,14 +1,8 @@
 import Link from "next/link";
 
-import { QuantitySelector, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { Title } from "@/components";
 import Image from "next/image";
-
-const productInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-];
+import { ProductsInCart } from "./ui/ProductsInCart";
 
 export default function CheckoutPage() {
   return (
@@ -25,26 +19,7 @@ export default function CheckoutPage() {
               Editar carrito
             </Link>
             {/*Items */}
-            {productInCart.map((product) => (
-              <div key={product.slug} className="flex mb-5">
-                <Image
-                  src={`/products/${product.images[0]}`}
-                  width={100}
-                  height={100}
-                  style={{
-                    width: "100px",
-                    height: "100px",
-                  }}
-                  alt={product.title}
-                  className="mr-5 rounded"
-                />
-                <div>
-                  <p>{product.title}</p>
-                  <p>${product.price} x 3</p>
-                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
-                </div>
-              </div>
-            ))}
+            <ProductsInCart />
           </div>
 
           {/*Checkout - resumen de orden*/}
