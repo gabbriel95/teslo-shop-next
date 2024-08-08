60331e789e85769149b463ca9ce44bd99e3e09a1
Gabriel <juanmendez0013@gmail.com>
2024-08-07 22:30:47 -0300

    feat: actualizar seed

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -10,12 +10,17 @@ async function main() {
     prisma.category.deleteMany(),
   ]);*/
 
+  await prisma.orderAddress.deleteMany();
+  await prisma.orderItem.deleteMany();
+  await prisma.order.deleteMany();
+
   await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.country.deleteMany();
+
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
-  await prisma.country.deleteMany();
 
   const { categories, products, users } = initialData;
 
